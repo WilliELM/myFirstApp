e407440804be3621fc3080178a27fdcbbc1a1d00
WilliELM <will585g@stud.kea.dk>
2023-02-27 10:45:40 +0100

    added override to resume and pause

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.myapplication;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+String TAG = this.getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity","App is up and running");
+    }
+    @Override
+    protected void onPause () {
+        super.onPause();
+        Log.d(TAG, "onPause: App is now on pause");
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: App is now resumed");
     }
 }
